1588bf0609394dcebd5b406f596426ea6a21973f
jjbonnin <juanjo.bonnin.uoc@gmail.com>
2019-12-16 17:16:57 +0100

    llista de paraules

--- exer01.py
+++ exer01.py
@@ -1,2 +1,35 @@
-dni="aa"
-print(dni)
+num=""
+num = input("Introdueix el nombre de paraules: ")
+value=int(num)
+cadenes=[]
+for i in range(value):
+  cadena=input("introduce elemento: ")
+  cadenes.append(cadena)
+  
+print(num, value, cadena, cadenes)
+
+
+# cadenes=[]
+while (True):
+  cadena=input("introduce elemento: ")
+  if cadena == "":
+    break
+  else:  
+    cadenes.append(cadena)
+num = len(cadenes)  
+
+
+acercar=input("paraula a cercar: ")
+cuantes=cadenes.count(acercar)
+
+
+
+
+
+while (cuantes > 0):
+  print(num, cuantes, cadena, cadenes)
+  cadenes.remove(acercar)
+  cuantes=cadenes.count(acercar)
+  
+
+print(num, cuantes, cadena, cadenes)
